12298f97f70bb88832caa8a3cc8eabf724fb86e8
nirvair09 <chandradeep262003@gmail.com>
2025-09-16 23:58:52 +0530

    "cocktail page added with animation"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Hero/>
+      <Cocktails/>
     </main>
   );
 };
