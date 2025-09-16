142f4871f897907b1e57538dfe9fc4e0d4082df6
nirvair09 <chandradeep262003@gmail.com>
2025-09-16 19:32:19 +0530

    "navbar added and public folder replaced for images and logo"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="flex-center">
-      <h1 className="text-2xl text-indigo-300">hello tailwind</h1>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
   );
 };
 
