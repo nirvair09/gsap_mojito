ef1ab6928ff40364ba25f88087f389fd2d528247
nirvair09 <chandradeep262003@gmail.com>
2025-09-16 19:12:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex-center">
+      <h1 className="text-2xl text-indigo-300">hello tailwind</h1>
+    </div>
+  );
+};
+
+export default App;
