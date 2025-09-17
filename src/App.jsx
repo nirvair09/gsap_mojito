4615134a641bd2009e789c731a925e7bd423cb7b
nirvair09 <chandradeep262003@gmail.com>
2025-09-17 09:17:23 +0530

    "art component added"

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <Cocktails/>
       <About/>
+      <Art/>
     </main>
   );
 };
